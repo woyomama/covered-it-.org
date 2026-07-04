79b3893efbcecd2eb2e635fb440bf67ae25c50b1
woyomama <shaikh.2121008@gmail.com>
2026-07-05 03:48:34 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import "@/index.css";
 import App from "@/App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60_000,
+      staleTime: 60000,
       refetchOnWindowFocus: false,
     },
   },
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
